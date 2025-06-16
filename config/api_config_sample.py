5fd7c6db65c7f949a2482828f5204306cc26c920
simsimi2143 <51520114+simsimi2143@users.noreply.github.com>
2025-06-16 09:41:40 -0400

    Update api_config_sample.py
    config api_key

--- config/api_config_sample.py
+++ config/api_config_sample.py
@@ -1,3 +1,3 @@
 # Configuración de API (renombrar a api_config.py después)
-NVIDIA_API_KEY = "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"
+NVIDIA_API_KEY = "APY_KEY"
 PDF_PATH = "../data/Trabajo de Titulo FINAL.pdf"  # Ruta relativa al PDF
